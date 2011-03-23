23d25331f3bb7666531eb8cadaf3c58deb68cdb0
Damian Jaszczurowski <damian.jaszczurowski@gmail.com>
2011-03-23 22:14:21 +0100

    message

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WPPl::Application.config.secret_token = '81eb800b529aa76671b89a1a284ae0fbefb8e12abed8c29b03e907f9ed98b83dc67f76645610a19dbaef2aaaf138678f67d1682157d702676c2bc83614408df7'
