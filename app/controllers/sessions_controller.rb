bbb290b1ed258d54c66b1122cdc9c4a4576da262
Damian Jaszczurowski <damian.jaszczurowski@gmail.com>
2011-05-06 18:07:10 +0200

    powiadamianie o banie przy logowaniu

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,4 @@
+# encoding:utf-8
 class SessionsController < ApplicationController
 	helper_method :sign_in
 	helper_method :sign_user_out
@@ -7,17 +8,16 @@ class SessionsController < ApplicationController
   def destroy  
     session[:current_user_id] = nil
     session[:log_as_admin] = false;
-    redirect_to root_url, :notice => "Wylogowano!"
+    redirect_to root_url, :notice => get_logged_out_text
   end
+  
   
    
 
   def create  	
 		user = User.authenticate(params[:session][:username],params[:session][:password])
 		if user		
-			if(is_user_banned(user))
-				puts "dupa"
-				puts user.banned.to_s
+			if(is_user_banned(user))				
 				if(user.banned < Date.current())
 					user.banned = nil
 					user.save
@@ -26,14 +26,14 @@ class SessionsController < ApplicationController
 							sign_in(user)	
 							redirect_to news_managment_url
 						else					
-							redirect_to admin_url, :notice => "Podano login zwyklego uzytkownika"
+							redirect_to admin_url, :notice => get_faulty_login_text
 						end
 					else
 						sign_in(user)					
-						redirect_to :back, :notice => 'Zalogowany'
+						redirect_to :back, :notice => get_logged_in_text
 					end
-				else
-					redirect_to root_url
+				else					
+					redirect_to root_url, :notice => get_ban_text + user.banned.to_s
 				end	
 			else							
 				if(session[:log_as_admin])
@@ -41,16 +41,15 @@ class SessionsController < ApplicationController
 						sign_in(user)	
 						redirect_to news_managment_url
 					else					
-						redirect_to admin_url, :notice => "Podano login zwyklego uzytkownika"
+						redirect_to admin_url, :notice => get_faulty_login_text
 					end
 				else
 					sign_in(user)					
-					redirect_to :back, :notice => 'Zalogowany'
+					redirect_to :back, :notice => get_logged_in_text
 				end
 			end
-		else
-      flash.now[:error] = "Niepoprawne haslo lub login"			
-			redirect_to :back, :notice => 'Niepoprawne haslo lub login'
+		else     	
+			redirect_to :back, :notice => get_faulty_login_text
 		end
   end
   
@@ -71,5 +70,38 @@ class SessionsController < ApplicationController
   	def remember_token
   		cookies.signed[:remember_token] || [nil, nil]
   	end
+  	
+  	def get_ban_text	
+  		if is_english_language
+  			"Your account will be unblocked on"
+  		else
+  			"Twoje konto zostanie odblokowane za  "
+  		end
+  	end
+  	
+  	def get_faulty_login_text
+  		if is_english_language
+  			"Given password or login is not valid"
+  		else
+  			"Podane hasło lub login jest niepoprawne"
+  		end
+  	end
+  	
+  	def get_logged_in_text
+  		if is_english_language
+  			"Logged in"
+  		else
+  			"Zalogowano"
+  		end
+  	end
+  	
+  	def get_logged_out_text
+  		if is_english_language
+  			"Logged out"
+  		else
+  			"Wylogowano"
+  		end
+  	end
+  	
 end
 
